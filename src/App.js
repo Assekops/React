c9f21056178fc99d2114ebd52f1bf8cc7aa2dd83
Assefa Tadesse <assekopsynwa@gmail.com>
2023-05-27 11:18:25 -0500

    Campsite Cards Added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import React from 'react';
 import './App.css';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
+import CampsitesList from './features/campsites/CampsitesList.js';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color='primary' sticky='top' expand='md'>
-        <Container>
-          <NavbarBrand href='/'>
-            <img src={NucampLogo} alt='nucamp logo' />
-          </NavbarBrand>
-        </Container>
-      </Navbar>
-      I'm Ready for workshop!
+      <Header />
+      <CampsitesList />
+      <Footer />
     </div>
   );
 }
