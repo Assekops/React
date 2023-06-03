94f12147c55ceeaad143309f0063dd0700b6a3bf
Assefa Tadesse <assekopsynwa@gmail.com>
2023-06-03 07:35:34 -0500

    Display Partners

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
@@ -16,6 +17,7 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='contact' element={<ContactPage />} />
         <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route path='about' element={<AboutPage />} />
         <Route
           path='directory/:campsiteId'
           element={<CampsiteDetailPage />}
--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -3,19 +3,21 @@ import { useParams } from 'react-router-dom';
 import { selectCampsiteById } from '../features/campsites/campsitesSlice';
 import CampsiteDetail from '../features/campsites/CampsiteDetail';
 import CommentsList from '../features/comments/CommentsList';
+import SubHeader from '../components/SubHeader';
 
-const CampsiteDetailPage =()=>{
-   
+const CampsiteDetailPage = () => {
+
     const { campsiteId } = useParams();
     const campsite = selectCampsiteById(campsiteId);
-    
-    return(
+
+    return (
         <Container>
-        <Row>
-            <CampsiteDetail campsite={campsite} />
-            <CommentsList campsiteId={campsiteId} />
-        </Row>
-    </Container>
+            <SubHeader current={campsite.name} detail={true} />
+            <Row>
+                <CampsiteDetail campsite={campsite} />
+                <CommentsList campsiteId={campsiteId} />
+            </Row>
+        </Container>
     );
 };
 export default CampsiteDetailPage;
