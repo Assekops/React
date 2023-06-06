b24ad33e2849b3924f740857959e1d7c211624fa
Assefa Tadesse <assekopsynwa@gmail.com>
2023-06-05 21:56:06 -0500

    Partners list on about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,5 +1,6 @@
 import { Col, Row, Container, Card, CardBody, CardHeader } from "reactstrap";
 import SubHeader from "../components/SubHeader";
+import PartnersList from '../features/partners/PartnersList';
 
 const AboutPage = () => {
     return (
@@ -59,6 +60,7 @@ const AboutPage = () => {
             </Row>
             <Row className='row-content'>
                 <Col>
+                    <PartnersList />
                     <h3>Community Partners</h3>
                 </Col>
             </Row>
